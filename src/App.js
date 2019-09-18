81d355716aa0e5799303d8e277644aa149099e9e
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-09-18 18:58:36 +0200

    set page title

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     const steps = splitSteps(markdown);
     const title = getTitle(markdown);
     const subtitle = getSubtitle(markdown);
+    document.title = title;
     this.setState({ steps, subtitle, title }, Prism.highlightAll);
   }
 
